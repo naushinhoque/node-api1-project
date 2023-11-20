6f351a6f8f325a6cb8f7f14c23f51f6d39ca7adc
naushinhoque <fnmh@yahoo.com>
2023-11-20 15:56:52 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -1,14 +1,24 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-
+const User = require('./users/model')
 const server = express()
 
-server.get('/api/users', (req, res) => {
-    console.log('getting all users')
+server.get('/api/users:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
 })
 
 server.use('*', (req, res) => {
-    res.status(404).json({
+    res.status(400).json({
         message: 'not found'
     })
 })
